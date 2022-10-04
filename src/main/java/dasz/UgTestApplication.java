cd9e59b68608a5bcc358bf591f9dfe1b2f1b383c
dariuszszmycinski <szmytek89@gmail.com>
2022-10-04 11:00:09 +0200

    Initial commit, project setup from spring initializer

---
+++ src/main/java/dasz/UgTestApplication.java
@@ -0,0 +1,11 @@
+package dasz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UgTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UgTestApplication.class, args);
+    }
+}
